99aee1baddcf89f2bb2d4e85341a49f12be61d9c
Michael Rickert <mrickert@calipercorp.com>
2013-09-30 12:31:51 -0400

    Added a contact us page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -17,18 +17,28 @@ describe "StaticPages" do
       expect(page).to have_content('Help')
     end
       it "should have the right title" do
-    visit '/static_pages/home'
-    expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    visit '/static_pages/help'
+    expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
   end
   end
-  describe "About page" do
+  describe "About Page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
       it "should have the right title" do
-    visit '/static_pages/home'
-    expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    visit '/static_pages/about'
+    expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
   end
+  end
+  describe "Contact Page" do
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
+    end
+      it "should have the right title" do
+    visit '/static_pages/contact'
+    expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact Us")
+    end
   end
 end
